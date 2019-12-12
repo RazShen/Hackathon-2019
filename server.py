20bf3e6b10b56ef9570d3ba003c3d15ce203f646
raz.shenkman <shenkmanraz@gmail.com>
2019-12-12 12:00:03 +0200

    all works

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import config_pb2
-import struct
-
+import os
 app = Flask(__name__, template_folder=".",static_url_path='/static')
 
 
@@ -15,21 +14,16 @@ def edit_protobuf(metric_1, metric_2, fil, operator, val):
     ex_f.close()
     whole_pb = config_pb2.SiteConfig()
     whole_pb.ParseFromString(buf)
-    print whole_pb
     rule = config_pb2.AuditRule()
-    rule.id = 1
-    rule.filter = "Always == True"
-    field1 = rule.field.add()
-    field1 = metric_1
-    field2 = rule.field.add()
-    field2 = metric_2
-    rule.field.extend([metric_1,metric_2])
-    print rule
-    whole_pb.auditRules = whole_pb.auditRules.add()
-    whole_pb.auditRules.extend([rule])
+    rule.id = 7
+    rule.name = b"Hackathon Cool"
+    rule.filter =b"Always == True"
+    rule.field[:] = [metric_1.encode(), metric_2.encode()]
+    whole_pb.siteRulesConf.auditRules.append(rule)
     f = open("res", "wb")
     f.write(whole_pb.SerializeToString())
     f.close()
+    os.system("curl -k -X POST --data-binary @/Users/raz.shenkman/Documents/hackathon_2019/res 198.143.43.201/_Incapsula_Resource?XMSHLOMIT=955944")
 
 @app.route('/', methods=['POST', 'GET"'])
 def my_form_post():
@@ -38,7 +32,6 @@ def my_form_post():
     fil = request.form['fil']
     operator = request.form['op']
     val = request.form['val']
-    print metric_1, metric_2, fil, operator, val
     edit_protobuf(metric_1, metric_2, fil, operator, val)
     return render_template('index.html')
 
